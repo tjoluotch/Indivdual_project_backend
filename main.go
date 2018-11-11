5226fa681cc5fb9226c8e2ad5a162354bec32033
TJ Oluotch <trevorjoluotch@gmail.com>
2018-11-11 20:00:40 +0000

    refactored to include LogFileSetup()

--- main.go
+++ main.go
@@ -7,11 +7,7 @@ import (
 	"os"
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request){
-	w.Write([]byte("Individual project!"))
-}
-
-func main() {
+func LogFileSetup() {
 	// create a new file if one doesn't exists and append data to this file when writing.
 	file, err := os.OpenFile("info.log", os.O_RDWR |os.O_CREATE|os.O_APPEND, 0666)
 
@@ -25,13 +21,22 @@ func main() {
 	log.SetOutput(file)
 	// log with date time and file location
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+}
 
+func HomeHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Individual project!"))
+}
+
+func main() {
+
+	LogFileSetup()
 
 	router := mux.NewRouter()
 	// Routes consist of a Path and Handler function
 
 	// Router to HomeHandler
 	router.HandleFunc("/", HomeHandler)
+
 	log.Printf("server running on port %v", 6000)
 	//Server start listen on port 6000
 	log.Fatal(http.ListenAndServe(":6000", nil))
