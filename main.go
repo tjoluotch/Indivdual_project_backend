7ae549fa497df08522b54919cb0f04110856944f
TJ Oluotch <trevorjoluotch@gmail.com>
2018-11-11 19:54:41 +0000

    added log file

--- main.go
+++ main.go
@@ -12,13 +12,30 @@ func HomeHandler(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
-	file := os.OpenFile("info.log")
+	// create a new file if one doesn't exists and append data to this file when writing.
+	file, err := os.OpenFile("info.log", os.O_RDWR |os.O_CREATE|os.O_APPEND, 0666)
+
+	// if there's an error with the opening of the log file log Fatal
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	// set output to the file
+	log.SetOutput(file)
+	// log with date time and file location
+	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
+
+
 	router := mux.NewRouter()
 	// Routes consist of a Path and Handler function
 
 	// Router to HomeHandler
 	router.HandleFunc("/", HomeHandler)
-	// Server start listen on port 6000
+	log.Printf("server running on port %v", 6000)
+	//Server start listen on port 6000
 	log.Fatal(http.ListenAndServe(":6000", nil))
+
+
 
 }
