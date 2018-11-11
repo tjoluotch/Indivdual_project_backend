2ef0c7fec8f3edab6d58c895fcfe464d7b455936
TJ Oluotch <trevorjoluotch@gmail.com>
2018-11-11 18:55:40 +0000

    created server with HomeHandler

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Individual project!"))
+}
 
 func main() {
-	fmt.Println("hello Trev")
+	file := os.OpenFile("info.log")
+	router := mux.NewRouter()
+	// Routes consist of a Path and Handler function
+
+	// Router to HomeHandler
+	router.HandleFunc("/", HomeHandler)
+	// Server start listen on port 6000
+	log.Fatal(http.ListenAndServe(":6000", nil))
+
 }
