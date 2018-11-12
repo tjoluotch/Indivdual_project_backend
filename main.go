7a52d5c5e813d70b37582b4f079b7bc4b8c6678b
TJ Oluotch <trevorjoluotch@gmail.com>
2018-11-12 05:28:21 +0000

    added json encoding to struct

--- main.go
+++ main.go
@@ -9,9 +9,10 @@ import (
 )
 
 type User struct {
-	Name string
-	Unique_ID int64
-	Phone_no string
+	FirstName string `json:"first_name,omitempty"`
+	LastName string	 `json:"last_name,omitempty"`
+	U_ID string		 `json:"unique_id,omitempty"`
+	Phone_No string	 `json:"phone_no,omitempty"`
 }
 
 func LogFileSetup() {
@@ -30,7 +31,14 @@ func LogFileSetup() {
 	log.SetFlags(log.Ldate | log.Ltime | log.Llongfile)
 }
 
+// Get the form data entered by client; FirstName, LastName, phone Number,
+// assign the person a unique i.d
+// check to see if that user isn't in the database already
+// if they are send an error message with the a  'bad' response code
+// if they aren't in db add to db and send a message with success
 func SignUpEndpoint(response http.ResponseWriter, request *http.Request){
+	log.Print("Entered func SignUpEndpoint")
+	log.Printf("Request: %v", request)
 	response.Write([]byte("Individual project!"))
 }
 
@@ -45,9 +53,9 @@ func main() {
 	methods := handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE"})
 	origins := handlers.AllowedOrigins([]string{"*"})
 
-	router.HandleFunc("/signup", SignUpEndpoint).Methods("POST")
+	router.HandleFunc("/signup", SignUpEndpoint)
 	//log server running
-	log.Printf("server running on port %v", 6000)
-	log.Fatal(http.ListenAndServe(":6000", handlers.CORS(headers, methods, origins)(router)))
+	log.Printf("server running on port %v", 8080)
+	log.Fatal(http.ListenAndServe(":8080", handlers.CORS(headers, methods, origins)(router)))
 
 }
